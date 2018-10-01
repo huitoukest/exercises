d90228f4b30ab0cad937c8dc66423ac18c759be7
huitoukest <huitoukest@foxmail.com>
2018-10-01 22:10:01 +0800

    add babelrc

--- react/test001/src/index.js
+++ react/test001/src/index.js
@@ -3,7 +3,8 @@ console.log("ok 0")
 
 // 1.这里两个导入的时候，接收的成员名称必须这么些
 import React from 'react' //创建爱你组件，虚拟dom元素，生命周期
-import ReactDom from 'react-dom' //把创建好的组件和虚拟DOM放到页面上展示
+//这里的React-dom推荐写为ReactDOM
+import ReactDOM from 'react-dom' //把创建好的组件和虚拟DOM放到页面上展示
 
 // 2.创建虚拟DOM元素
 
@@ -25,6 +26,10 @@ const myDiv = React.createElement('div',null,'这是一个div元素',myh1)
 //参数2 = 一个dom元素，不是选择器，指定页面上的容器
 // ReactDom.render(myDiv,document.getElementById('app'))
 
-const myTest = <div> aaa </div>
-ReactDom.render(myTest,document.getElementById('app'))
+//注意： 在js文件中，默认不能够写类似于HTML的标记，不过可以使用babel来转换这些JS中的标签；
+//这种在JS中混合写入类似于HTML的语法，叫做JSX语法。即符合XML规范的JS。可以通过babel将JSX语法转为JS语法。
+//注意：所以JSX的本质是运行的时候转为js语法。
+
+const myTest = <div id="myDiv" title="div aaa" > aaa 测试内容 myTests </div>
+ReactDOM.render(myTest,document.getElementById('app'))
 
--- react/test001/webpack.config.js
+++ react/test001/webpack.config.js
@@ -13,10 +13,19 @@ const htmlPlugin = new HtmlWebPackPlugin({
 
 // 向外部暴露一个打包的配置对象；node语法，因为webpack是基于Node构建的，所以
 //webpack支持所有node api与之语法
+//webpack 默认只能打包处理 .js后缀类型的文件；像其余的.png .vuew等文件类型无法处理，
+//所以需要配置一些第三方的loader模块
 module.exports = {
       mode: 'development' ,//development or production
       //在webpack 4.x中，默认的打包入口路径是src -> index.js
-      plugins:[
+      plugins:[ //plugins中是配置webpack的插件
             htmlPlugin
-      ]
+      ],
+      module:{ //module是配置第三方依赖包和扩展的匹配配置
+           rules:[ //第三方匹配规则,这里匹配jsx和js格式的文件使用babel-loader来，同时如果使用到多个loader可以用数组配置
+                  // 其次，一定要exclude ，配置排除/node_modules/文件夹下的内容
+                 { test: /\.js|\.jsx/ ,use: 'babel-loader' , exclude: /node_modules/ }
+           ]
+
+      }
 } 
