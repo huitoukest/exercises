1b1cbabaf9d20f30bcf167163893c5c242bf1099
huitoukest <huitoukest@foxmail.com>
2018-10-28 21:36:28 +0800

    add
    Signed-off-by: huitoukest <huitoukest@foxmail.com>

--- react/test001/src/components/Cmt.2.jsx
+++ react/test001/src/components/Cmt.2.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import CmtItem2 from '@/components/CmtItem.2.jsx'
 
+//手动导入bootstrap下面的css,默认优先查找node_modules下面的文件夹
+//也可以手动配置，但是如果此包已经被安装到了node_modules下面，那么可以直接引入
+//import bootcss from 'bootstrap/dist/css/bootstrap.css'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+//console.info(bootcss)
+
 export default class Cmt2 extends React.Component{
 
     constructor(){
@@ -33,6 +41,15 @@ export default class Cmt2 extends React.Component{
                     this.state.cmList.map(item => 
                         <CmtItem2 {...item}  key= {item.id}></CmtItem2>
                     )
+                }
+                
+                <button className="btn btn-primary">bootstrap按钮</button>
+                <div className="panel panel-primary">bootstrap panel</div>
+                {
+                    /**
+                    如果不配置
+                    <button className={[bootcss.btn,bootcss['btn-primary']].join(' ') }>bootstrap按钮</button>
+                    */
                 }
         </div>
     }
