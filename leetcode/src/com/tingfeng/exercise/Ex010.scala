7f1128e5130170c02bc65fb947693fa26379d36e
wanggang <wanggang@youxuepai.com>
2018-10-31 17:59:31 +0800

    back

--- leetcode/src/com/tingfeng/exercise/Ex010.scala
+++ leetcode/src/com/tingfeng/exercise/Ex010.scala
@@ -10,11 +10,22 @@ object Ex010 {
   var letter = Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 
   def main(args: Array[String]): Unit = {
-    val sArray = Array("aab","aa","aa","mississippi");
-    val pattern = Array("c*a*b","a*","aa","mis*is*p*.")
+    val testLength = 10000
+    val pairStr = generatorSimpleRegExpStr(testLength)
+    var isBreak = false
+    for(i <- 0 until testLength if !isBreak){
+      val pa = pairStr(i)
+      print(i + "\t")
+      println(pa)
+      if(!isMatch(pa._2,pa._1)){
+        isBreak = true
+      }
+    }
+    /*val sArray = Array("");
+    val pattern = Array("z*")
     for(i <- 0 until(sArray.length)){
       println(isMatch(sArray(i),pattern(i)))
-    }
+    }*/
   }
 
   def isMatch(s: String, p: String): Boolean = {
@@ -23,6 +34,10 @@ object Ex010 {
 
         if(chars.length <= 0 && patterns.length <= 0 ){ //匹配空
             true
+          }else if(chars.length <= 0 && patterns.length > 0 ){
+            if(patterns.length > 1 && patterns(1) == '*'){
+              true
+            }else false
           } else if(chars.length > 0 && patterns.length > 0 ){ //匹配有正则表达式的情况
           if(patternPosition >= patterns.length && chars.length > strPosition){
             false
@@ -80,27 +95,57 @@ object Ex010 {
 
     /**
       * 按照规则产生简单的正则表达式,转义符号只包含点号和星号
-      * @param length
-      * @return
+      * @param length 产生length条记录
+      * @return  Tuple2(正则表达式，字符串内容)
       */
-    def generatorSimpleRegExpStr(length : Int):Tuple2[String,String] = {
+    def generatorSimpleRegExpStr(length : Int):IndexedSeq[Tuple2[String,String]] = {
         val sb = new StringBuilder
-        for (i <- 0 to length){
-          val typeValue = ThreadLocalRandom.current().nextInt(4)
-          if(typeValue == 0){
-            val randomPosition = ThreadLocalRandom.current().nextInt(26)
-            sb.append(letter(randomPosition))
-          }else if(typeValue == 1){
-            sb.append(".")
-          }else if(typeValue == 2){
-            sb.append("*")
-          }else if(typeValue == 3){
-            sb.append(".*")
+        val content = new StringBuilder
+        val charSb = new StringBuilder
+
+        def makeOneStr(len: Int):String={
+            charSb.setLength(0)
+            for(i <- 0 until len){
+              val randomPosition = ThreadLocalRandom.current().nextInt(26)
+              charSb.append(letter(randomPosition))
+            }
+          charSb.toString()
+        }
+
+        def makeStr():Tuple2[String,String] = {
+          sb.setLength(0)
+          content.setLength(0)
+          val strLength = ThreadLocalRandom.current().nextInt(10) //一个大概的长度
+          for (i <- 0 to strLength){
+            val typeValue = ThreadLocalRandom.current().nextInt(4)
+            typeValue match {
+              case 0 => {
+                val tmp = ThreadLocalRandom.current().nextInt(3) + 1
+                val str = makeOneStr(tmp)
+                sb.append(str)
+                content.append(str)
+              }
+              case 1 => {
+                sb.append(".")
+                content.append(makeOneStr(1))
+              }
+              case 2 => {
+                val str = makeOneStr(1)
+                sb.append(s"$str*")
+                for(j <- 0 until ThreadLocalRandom.current().nextInt(3)){
+                  content.append(str)
+                }
+              }
+              case 3 => {
+                sb.append(".*")
+                for(j <- 0 until ThreadLocalRandom.current().nextInt(3)){
+                  content.append(makeOneStr(1))
+                }
+              }
+            }
           }
+          (sb.toString(),content.toString())
         }
-
-        def makeStr():String = {
-
-        }
+      for(i <- 0 until length) yield makeStr()
     }
 }
